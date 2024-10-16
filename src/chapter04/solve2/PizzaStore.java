c9ae91623c7b83cb6837d605c387fff6dfd53670
HamSeungChan <head0618@naver.com>
2024-10-16 15:14:24 +0900

    팩토리 메소드 패턴 test

--- src/chapter04/solve2/PizzaStore.java
+++ src/chapter04/solve2/PizzaStore.java
@@ -5,6 +5,10 @@ package chapter04.solve2;
  *               시가코 피자 스타일의 치즈피자, 페퍼로피 피자
  *               뉴욕 피자 스타일의 치즈피자, 페퍼로니 피자
  *               ->> SimplePizzaFactory 를 대신해 새로운 팩토리 클래스를 2개 생성
+ *
+ *   해결 방법 -> PizzaStore의 서브 클래스에서 구상 클래스의 인스턴스를 만드는 일을 맡는다.
+ *
+ *
  * */
 
 
@@ -25,6 +29,7 @@ public abstract class PizzaStore {
     }
 
     // 추상 메소드 형식으로 팩토리 메소드 사용
+    // 팩토리 메소드를 활용하면 클라이언트에서 실제고 생성되는 구상 객체가 무엇인지 알 수 없게 한다.
     abstract Pizza createPizza(String type);
 
 }
--- src/chapter04/solve2/ny/NYCheesePizza.java
+++ src/chapter04/solve2/ny/NYCheesePizza.java
@@ -3,4 +3,14 @@ package chapter04.solve2.ny;
 import chapter04.solve2.Pizza;
 
 public class NYCheesePizza extends Pizza {
+
+    public NYCheesePizza() {
+
+        name = "뉴욕 스타일 소스와 치즈 피자";
+        dough = "씬 크러스트 도우";
+        sauce = "마리나라 소스";
+
+        toppings.add("잘게 썬 레지아노 치즈");
+    }
+
 }
