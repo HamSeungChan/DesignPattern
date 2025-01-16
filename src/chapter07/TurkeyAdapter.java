4a4e24a6b34265fa7df3c7c0dc281c0e414930b6
HamSeungChan <head0618@naver.com>
2025-01-16 16:06:58 +0900

    TurkeyAdapter 구현 Duck -> TurkeyAdapter -> Turkey

---
+++ src/chapter07/TurkeyAdapter.java
@@ -0,0 +1,24 @@
+package chapter07;
+
+// 클라이언트에서 원하는 인터페이스를 구현
+public class TurkeyAdapter implements Duck {
+
+    // 기존 형식의 객체 래퍼런스
+    private Turkey turkey;
+
+    public TurkeyAdapter(Turkey turkey) {
+        this.turkey = turkey;
+    }
+
+    @Override
+    public void quack() {
+        turkey.gobble();
+    }
+
+    @Override
+    public void fly() {
+        for (int i = 0; i < 5; i++) {
+            turkey.fly();
+        }
+    }
+}
